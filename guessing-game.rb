3524c91a8305f41b75eadba1d2efa05c2c1d850b
danielamstutz <daniel.r.amstutz@gmail.com>
2016-11-01 07:07:50 -0500

    submission

--- guessing-game.rb
+++ guessing-game.rb
@@ -1,19 +1,23 @@
-##############################################################################
-#
-# Update this script to:
-#
-# - Ask for the player's name
-# - Greet the player by name and explain the game
-# - Ask for the maximum number to select as the secret number
-# - Pick a secret number at random between 1 and the number entered
-# - Ask the player (by name) to guess a number
-# - Tell the player if they guessed correctly
-# - If they guessed the number correctly, congratulate them for winning
-#   and then tell them how many guesses it took for them to win.
-# - If they did not guess correctly, tell them whether their guess was
-#   too high or too low.
-#
-# DON'T FORGET:
-#   Use methods to break down the problem and make the code easier to read.
-#
-#############################################################################
+puts 'whats your name?'
+name=gets.chomp
+
+puts 'Hi ' +name+ '!'
+puts
+puts 'Pick a maximum for a secret number?'
+number=gets.chomp.to_i
+secretnum= rand(1..number)
+
+
+loop{
+puts +name+ ' please guess the number?'
+answer=gets.chomp.to_i
+if answer == secretnum
+  puts 'you got it!'
+elsif answer<secretnum
+  puts 'try a larger number.'
+
+else answer>secretnum
+  puts 'try  a smaller number.'
+
+end
+}
